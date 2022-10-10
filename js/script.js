7ff15471cce799c77f1d1c13789425daf36726d3
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-10 16:10:58 +0200

    Bonus 1

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 
 //  codice che  stampi in console i numeri da 1 a 100
 for(let i = 1; i <= 100; i++){
+    const container = document.querySelector(".container");
+    const elementNumb = document.createElement("div");
+    elementNumb.classList.add("elementoNumb");
+
     if(( i % 3 == 0 ) && ( i % 5 == 0 )){
         // Stampa FizzBuzz se multiplo di 3 e 5
         console.log("FizzBuzz");
@@ -18,4 +22,6 @@ for(let i = 1; i <= 100; i++){
         // se non è multipplo di 3 e 5
         console.log(i);
     }
+    elementNumb.append(i);
+    container.append(elementNumb);
 }
